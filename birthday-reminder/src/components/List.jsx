7f670eafc0ba961a92a43a540f9110407b915f58
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-02-25 15:34:17 +0400

    COMPLETED: BIRTHDAY REMINDER APP -- cleared defaults, added css, example data and completed List components + added to App.jsx to render.

---
+++ birthday-reminder/src/components/List.jsx
@@ -0,0 +1,24 @@
+import { PropTypes } from 'prop-types';
+
+export default function List({ people }) {
+	return (
+		<>
+			{people.map((person) => {
+				const { id, name, age, image } = person;
+				return (
+					<article className="person" key={id}>
+						<img src={image} alt={name} />
+						<div>
+							<h4>{name}</h4>
+							<p>{age} years</p>
+						</div>
+					</article>
+				);
+			})}
+		</>
+	);
+}
+
+List.propTypes = {
+	people: PropTypes.array.isRequired,
+};
